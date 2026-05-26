12ae32769a2d3cda2e712c97b1443a59c11fa403
trungg4863257-crypto <trungg4863257@gmail.com>
2026-05-26 12:45:11 +0700

    Create Giophutgiay.h

---
+++ Giophutgiay.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef GIOPHUTGIAY_H
+#define GIOPHUTGIAY_H
+class GioPhutGiay {
+private:
+    int iGio;
+    int iPhut;
+    int iGiay;
+public:
+    void Nhap();
+    void Xuat();
+    void TinhCongThemMotGiay();
+};
+
+#endif
